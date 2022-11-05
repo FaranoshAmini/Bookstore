16fb84f6874e78b9a2270b484449b75899928712
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 19:23:59 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Categories from './components/Categories';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+       <Header />
+      <Routes>
+        <Route path='/categories' element={<Categories />} />
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
