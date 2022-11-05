dfa7f50dc5453f7dd253c94d5d7555de0fd42b97
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 20:44:10 +0430

    Added NavLink for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Bookstore CMS</h1>
-      <nav>
-        <ul>
-          <li>
-            Books
-          </li>
-          <li>
-            Categories
-          </li>
-        </ul>
+    <header className="header">
+       <nav className="navbar">
+        <NavLink to='/' className="navitem">Books</NavLink>
+        <NavLink to='/categories' className="navitem">Categories</NavLink>
       </nav>
     </header>
   );
