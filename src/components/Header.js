7710c61121b24a8e80e6f71af9056cd0b4ae9b9c
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 21:31:46 +0430

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+function Header() {
   return (
     <header className="header">
-       <nav className="navbar">
-        <NavLink to='/' className="navitem">Books</NavLink>
-        <NavLink to='/categories' className="navitem">Categories</NavLink>
+      <nav className="navbar">
+        <NavLink to="/" className="navitem">Books</NavLink>
+        <NavLink to="/categories" className="navitem">Categories</NavLink>
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
