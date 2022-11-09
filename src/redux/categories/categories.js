b358c133d7bbba51548b90e01a64638699e2315f
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-09 12:10:17 +0430

    Added reducer for categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,2 +1,13 @@
 // ACTIONS
 const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
+
+// REDUCER
+// eslint-disable-next-line default-param-last
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
