a5f2b464e52b72a297b1017dc09d6accaaf8bdd7
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-09 18:23:00 +0430

    Add functionality to check status in categories route

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
+function Categories() {
+  const categoriesStatus = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
 
+  const handleStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleStatus}>
+        Check Status
+      </button>
+      <p>{categoriesStatus}</p>
+    </div>
+  );
+}
 
 export default Categories;
