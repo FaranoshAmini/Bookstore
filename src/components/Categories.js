88153c74d3a36a5c3a2d3f77c48948448cacae9c
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-09 18:22:13 +0430

    Added checkStatus in Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-function Categories() {
-  return (
-    <div>
-      <button type="button">
-        Check Status
-      </button>
-    </div>
-  );
-}
+
 
 export default Categories;
