de0148eb6dfcfcc68cc648841675ef6a74ab6335
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-09 17:38:32 +0430

    Added store in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
