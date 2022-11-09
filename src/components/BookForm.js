586b5f21f4d95dea0de9c98582c11b3a07efbd64
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-09 17:56:53 +0430

    Add functionality to add book using dispatch, actions and reducer

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function BookForm() {
+  const dispatch = useDispatch();
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    dispatch(addBook(e.target.title.value, e.target.author.value));
+    e.target.reset();
+  };
+
   return (
     <section className="book-form">
       <h2>Add a Book</h2>
-      <form>
+      <form onSubmit={handleAddBook}>
         <input type="text" name="title" placeholder="Enter name of the book" />
-        <input type="text" name="author" placeholder="Enter name of the author" />
+        <input
+          type="text"
+          name="author"
+          placeholder="Enter name of the author"
+        />
         <button type="submit">Add Book</button>
       </form>
     </section>
