99d2fed894dfd51c2767cf7fc5a16fd3eb25e74e
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-11 12:36:06 +0430

    Implement changes according to reviewer feedbacks

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
 function BookForm() {
   const dispatch = useDispatch();
 
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleInput = (e) => {
+    if (e.target.name === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setAuthor(e.target.value);
+    }
+    return e.target.value;
+  };
   const handleAddBook = (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const author = e.target.author.value;
-
     if (title && author) {
       dispatch(addBook(e.target.title.value, e.target.author.value));
       e.target.reset();
@@ -20,11 +28,12 @@ function BookForm() {
     <section className="book-form">
       <h2>Add a Book</h2>
       <form onSubmit={handleAddBook}>
-        <input type="text" name="title" placeholder="Enter name of the book" />
+        <input type="text" name="title" placeholder="Enter name of the book" onClick={handleInput} />
         <input
           type="text"
           name="author"
           placeholder="Enter name of the author"
+          onClick={handleInput}
         />
         <button type="submit">Add Book</button>
       </form>
