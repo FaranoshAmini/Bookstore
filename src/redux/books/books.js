c7cd397237a191222ab272df39f795d14bd21f02
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-11 17:11:05 +0430

    Update addBook to add new book via API call

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,32 +1,53 @@
-/* eslint-disable default-param-last */
+import { v4 as uuidv4 } from 'uuid';
+
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const apiKey = 'vEEu5G5Iqrs3wYfAbg4u';
+const requestedURL = `${baseURL}/apps/${apiKey}/books`;
+
 // ACTIONS
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const initialState = [];
-const bookId = 0;
-
-// REDUCER
-const booksReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return [...state, action.paylod];
-    case REMOVE_BOOK:
-      return [...state.filter((book) => book.id !== action.payload.id)];
-    default:
-      return state;
-  }
-};
+const initialState = [
+  {
+    id: 1,
+    title: 'Hear Yourself',
+    author: 'Prem Rawat',
+    category: 'Motivational',
+  },
+  {
+    id: 2,
+    title: 'Business of Sports: The Winning Formula for Success',
+    author: 'Vinit Karnik',
+    category: 'Motivational',
+  },
+  {
+    id: 3,
+    title: 'Great American Hero',
+    author: 'Marian Anderson',
+    category: 'Historical',
+  }, 
+];
 
 // ACTIONS CREATOR
-export const addBook = (title, author) => ({
-  type: ADD_BOOK,
-  paylod: {
-    id: bookId + 1,
+export const addBook = (title, author) => async (dispatch) => {
+  const book = {
+    item_id: uuidv4(),
     title,
     author,
+    category: 'Action',
   },
-});
+};
+
+await fetch(requestedURL, {
+  method: 'POST',
+  body: JSON.stringify(book),
+  headers: { 'Content-type': 'application/json; charset=UTF-8' },
+})
+  .then(() => dispatch({
+    type: ADD_BOOK,
+    payload: book,
+  }));
 
 export const removeBook = (id) => ({
   type: REMOVE_BOOK,
@@ -34,5 +55,4 @@ export const removeBook = (id) => ({
     id,
   },
 });
-
 export default booksReducer;
